1706ffa0b3f075a2069cf4d1caeaadf4c953579a
chelseysho <chelseyshortellisbm4km@hotmail.com>
2024-09-20 15:41:09 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("5147ae7b922b6446479500cc69dddbf4eeb3bea1e6cbaa82bd1c31aa403d6b3f", "5147ae7b922b6446479500cc69dddbf4eeb3bea1e6cbaa82bd1c31aa403d6b3f") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
